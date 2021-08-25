4bbc8d59ea1aed14afde0b0bfe71a47b6c9d734c
Vipul Sharma <panditsfire@gmail.com>
2021-08-25 12:14:40 +0530

    Updates

---
+++ constants/data.js
@@ -0,0 +1,131 @@
+const userData = {
+  githubUsername: "vipulksharma",
+  name: "Vipul Sharma",
+  designation: "Frontend Architect",
+  avatarUrl: "/avatar.jpeg",
+  email: "panditsfire@gmail.com",
+  phone: "+91 9015713889",
+  address: "Noida, UP, India.",
+  projects: [
+    {
+      title: "Paytm Home",
+      link: "https://paytm.com",
+      imgUrl: "/paytm.png",
+    },
+    {
+      title: "Paytm Foundation",
+      link: "https://www.paytmfoundation.org/",
+      imgUrl: "/foundation.png",
+    },
+    {
+      title: "Oxygen Donation Campaign",
+      link: "https://paytm.com/oxygen-campaign#/donations",
+      imgUrl: "/donation.png",
+    },
+    {
+      title: "Tokopedia",
+      link: "https://tokopedia.com",
+      imgUrl: "/tokopedia.png",
+    },
+    {
+      title: "Paytm Movies",
+      link: "https://paytm.com/movies",
+      imgUrl: "/movies.png",
+    },
+    {
+      title: "Paytm Bus",
+      link: "https://paytm.com/bus-tickets",
+      imgUrl: "/paytmbus.png",
+    },
+    {
+      title: "Store Hippo",
+      link: "https://storehippo.com",
+      imgUrl: "/storehippo.png",
+    },
+    {
+      title: "Rangri",
+      link: "https://rangri.com.br/",
+      imgUrl: "/rangri.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Web architect, taking care of highly scalable product impacting over millions of people in India",
+    description: [
+      `I've been developing full-stack application since I was in college.`,
+      `Writing code that can be reused and product that can scale without issues for millions.`,
+      `Well versed with React, Javascript, Node, Html, CSS. Trying a few new things of late. Very new to firebase and related stuff.`,
+    ],
+    currentProject: "Paytm",
+    currentProjectUrl: "https://paytm.com",
+  },
+  experience: [
+    {
+      title: "Tech Lead + Senior Tech Lead",
+      company: "Paytm",
+      year: "2018",
+      companyLink: "https://paytm.com",
+      desc: "Second term with Paytm. Revamped home pages along with multiple web pages also getting served on application. Check your orders section.",
+    },
+    {
+      title: "Tech Lead",
+      company: "Tokopedia",
+      year: "2017",
+      companyLink: "https://tokopedia.com",
+      desc: "Worked on website optimisation",
+    },
+    {
+      title: "Senior Software Engineer + Tech Lead",
+      company: "Paytm",
+      year: "2015-2017",
+      companyLink: "https://paytm.com",
+      desc: "Made multiple verticals for Paytm. Travel - bus, flights, hotels. O2O - Movies, events, parks etc",
+    },
+    {
+      title: "Software Engineer",
+      company: "Paytm",
+      year: "2014",
+      companyLink: "https://paytm.com",
+      desc: "Developed and maintained marketplace for paytm.com. Currently Paytmmall",
+    },
+    {
+      title: "Web Developer",
+      company: "Store Hippo (Manusis)",
+      year: "2012",
+      companyLink: "https://storehippo.com",
+      desc: "MEAN stack developer along with a number of projects done on JQUERY.",
+    },
+    {
+      title: "Internship",
+      company: "ONLY DID ON PAPER",
+      year: "2011",
+      companyLink: "#",
+      desc: "Did internship on paper just for sake of doing. Playing cricket and CS were priorities then.",
+    },
+    {
+      title: "Graduation",
+      company: "ABES IT, Ghaziabad.",
+      year: "2012",
+      companyLink: "https://abesit.in/",
+      desc: "Major in Computer Science and Engineering with a CGPA of 6.8. Struggle with marks is real.",
+    },
+    {
+      title: "High School",
+      company: "Saharanpur Public School, Saharanpur.",
+      year: "2006",
+      companyLink: "https://www.saharanpurpublicschool.com/",
+      desc: "Somehow passed it ",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1G4YRI5c4juNCQX3h7Kfg9uo0QbbcrLln/view",
+  socialLinks: {
+    instagram: "https://instagram.com/thetravellerpandit",
+    twitter: "https://twitter.com/panditsfire",
+    linkedin: "https://www.linkedin.com/in/vipul-sharma-77489433",
+    github: "https://github.com/vipulksharma",
+    facebook: "https://facebook.com/thetravellerpandit",
+  },
+};
+
+export default userData;
