929edf8a5e4e74386464e3235ad16b1df13ded80
Vipul Sharma <panditsfire@gmail.com>
2021-10-07 08:07:07 +0530

    adding basic code

--- src/components/reactplayer.js
+++ src/components/reactplayer.js
@@ -5,8 +5,22 @@ export default class Product extends Component {
 
     state = {
         pip: false,
-        url: "https://dawchihliou.github.io/react-use-pip/video-sample.mp4"
+        url: "https://dawchihliou.github.io/react-use-pip/video-sample.mp4",
+        login: false,
     } 
+
+    getMobileOperatingSystem = () => {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return 'iOS';
+        }
+        if (/android/i.test(userAgent)) {
+            return 'Android';
+        }
+        return 'unknown';
+
+    }
 
     handleEnablePIP = () => {
         this.setState({
@@ -17,13 +31,42 @@ export default class Product extends Component {
     handleTogglePIP = () => {
         this.setState({
             pip: !this.state.pip
-        })
+        });
     }
 
     handleDisablePIP = () => {
         this.setState({
             pip: false
         })
+    }
+
+    checkLogin = () => {
+        if (!this.state.login) {
+            // Logic to call login 
+            // enable pip
+            if (this.getMobileOperatingSystem() === 'iOS') {
+                this.setState({
+                    pip: true
+                })
+            } else if (this.getMobileOperatingSystem() === 'Android'){
+                //call android bridge to enable pip
+                if (window["JSBridge"]) {
+                    window["JSBridge"].ticketEnablePIP(true);
+                }else {
+                    alert('bridge not found');
+                }
+            } else {
+                this.handleTogglePIP();
+            }
+            window.addEventListener('message', (event) => {
+                alert(`Received message: ${event.data}`);
+                // if logged in
+                // based on event data from app call login api to get user details.
+                this.handleDisablePIP();
+            });            
+        } else if (this.getMobileOperatingSystem() === 'unknown'){
+            this.handleTogglePIP();
+        }
     }
 
 
@@ -41,7 +84,7 @@ export default class Product extends Component {
             onDisablePIP={this.handleDisablePIP} 
             stopOnUnmount={false} />
         {ReactPlayer.canEnablePIP(url) && 
-        <button onClick={this.handleTogglePIP}>{
+        <button onClick={this.checkLogin}>{
             pip ? 'Disable PIP' : 'Enable PIP'
         }</button>}
       </div>
