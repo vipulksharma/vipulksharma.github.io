7bdbaa0102decfe4f463ca0a191f4bb4faa50eb4
Vipul Sharma <panditsfire@gmail.com>
2021-10-08 15:56:32 +0530

    fixes

--- src/components/reactplayer.js
+++ src/components/reactplayer.js
@@ -8,6 +8,14 @@ export default class ReactPlayerDemo extends Component {
         url: "https://dawchihliou.github.io/react-use-pip/video-sample.mp4",
         login: false,
     } 
+    componentDidMount() {
+        window.addEventListener('TL_NATIVE_BUTTON_PRESS', (event) => {
+            if (event && event.detail && event.detail.isPIP) {
+                this.handleTogglePIP();
+            }
+        });
+    }
+    
 
     getMobileOperatingSystem = () => {
         let standalone = window.navigator.standalone,
@@ -69,22 +77,18 @@ export default class ReactPlayerDemo extends Component {
             window.location.href = 'https://m.tiket.com/login';
             // App to intercept this call and check login and get login done and return event which we use to fetch username.
         }
+
         window.addEventListener('TL_SET_TOKEN', (event) => {
             // if logged in
             // based on event data from app call login api to get user details.
+            this.handleTogglePIP();
             sessionStorage.token = event.detail.token;
-            // Call local login api
-            if (ReactPlayer.canEnablePIP(this.state.url)) {
-                this.handleTogglePIP();
-            } else {
-                if (this.getMobileOperatingSystem() === 'Android'){
-                    //call android bridge to enable pip
-                    if (window["app"]) {
-                        window["app"].tiketTogglePIP(false);
-                    } 
-                    this.handleTogglePIP();
+            if (this.getMobileOperatingSystem() === 'Android'){
+                //call android bridge to enable pip
+                if (window["app"]) {
+                    window["app"].tiketTogglePIP(false);
                 } 
-            }
+            } 
         });            
     }
 
